10de137b837928516b5e4ecb0327aa3c8662c9bf
Yutao Han <y326han@uwaterloo.ca>
2021-07-31 06:37:44 +0000

    Update

--- observer.h
+++ observer.h
@@ -3,7 +3,7 @@
 
 class Observer {
     public:
-        // parameter n and m is used when necessary
+        // parameter n and m are used when necessary
         virtual void notify(int n=0, int m=0) = 0;     
         virtual ~Observer() = default;
 };
