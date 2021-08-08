74bf621136bf92eff10a005e8cef7489d05947ab
j254shi <j254shi@student.cs.uwaterloo.ca>
2021-08-08 01:55:56 -0400

    lv0

--- level_zero.h
+++ level_zero.h
@@ -2,9 +2,13 @@
 #define __LEVEL_ZERO_H__
 #include "level.h"
 #include <string>
+#include <fstream>
+#include <iostream>
 
 class Level0:public Level {
     string path;
+    std::ifstream infile;
+    int count;
     public:
         Level0(int n, string path);
         Block* createRandBlock(Board* board) override;
