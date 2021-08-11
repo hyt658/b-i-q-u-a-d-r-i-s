19a9280f3cd52671e94f9a649c944886596900aa
Zhongyi Guo <z242guo@uwaterloo.ca>
2021-08-11 06:09:46 -0400

    note

---
+++ board.d
@@ -0,0 +1,3 @@
+board.o: board.cc board.h cell.h commands.h subject.h observer.h block.h \
+ level_zero.h level.h iblock.h jblock.h lblock.h oblock.h sblock.h \
+ tblock.h zblock.h level_one.h level_two.h level_three.h
