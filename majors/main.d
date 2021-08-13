3a7a9195fd2faf5898feed8b16f2f29fd38485f2
Zhongyi Guo <z242guo@uwaterloo.ca>
2021-08-13 06:44:41 -0400

    note

--- majors/main.d
+++
@@ -1,6 +0,0 @@
-majors/main.o: majors/main.cc majors/biquadris.h \
- majors/../board/textdisplay.h majors/../board/board.h \
- majors/../board/cell.h majors/../board/../majors/commands.h \
- majors/../board/../subject+observer/subject.h \
- majors/../board/../subject+observer/observer.h \
- majors/../board/../subject+observer/observer.h
