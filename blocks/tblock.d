827177eb39091e689e5753072ba0e3d5bc3eb76c
Zhongyi Guo <z242guo@uwaterloo.ca>
2021-08-13 02:51:48 -0400

    note

--- blocks/tblock.d
+++
@@ -1,6 +0,0 @@
-blocks/tblock.o: blocks/tblock.cc blocks/tblock.h blocks/block.h \
- blocks/../subject+observer/observer.h \
- blocks/../subject+observer/subject.h \
- blocks/../subject+observer/observer.h blocks/../board/cell.h \
- blocks/../board/../majors/commands.h \
- blocks/../board/../subject+observer/subject.h
